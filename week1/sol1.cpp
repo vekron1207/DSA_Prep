8e1d012aee92c8e14818b8a30f8cd374ca3cff11
vekron <varun.kashyap1207@gmail.com>
2024-02-20 10:12:03 +0530

    sol1: Copied: Need to Study

---
+++ week1/sol1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[5];
+    cout << "Enter 5 numbers: " << endl;
+
+    for (int i = 0; i < 5 : i++)
+    {
+        cin >> num[i];
+    }
+    cout << "The number are: ";
+
+    for (int n = 0; n < 5; ++n)
+    {
+        cout << num[n] << " ";
+    }
+    return 0;
+}
