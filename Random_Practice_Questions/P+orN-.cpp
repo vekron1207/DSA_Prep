ae1fb461521ac161d3922de15d5d1ef4c33b584e
vekron <varun.kashyap1207@gmail.com>
2024-02-27 10:06:33 +0530

    Leaning Update: 27/02/2024

---
+++ Random_Practice_Questions/P+orN-.cpp
@@ -0,0 +1,20 @@
+// To find is a number is negative or positive.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "Enter a number: ";
+    cin >> i;
+
+    if (i > 0)
+    {
+        cout << "Positve number, right there" << endl;
+    }
+    else
+    {
+        cout << "Negative number it is.";
+    }
+}
